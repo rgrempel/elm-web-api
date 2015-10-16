7ed7cc7208cbb8bec4fa0f97c0201e8c2f1372bb
Ryan Rempel <rgrempel@gmail.com>
2015-10-16 17:33:23 -0500

    Create separate tests for 0.15 and 0.16, and test from command-line.

--- test/src/Browser.elm
+++ test/src/Browser.elm
@@ -3,8 +3,8 @@ module Main where
 import Signal exposing (Signal, Mailbox, mailbox, constant, send)
 import Task exposing (Task, andThen, sequence)
 import Graphics.Element exposing (Element)
-import ElmTest.Test exposing (..)
 import ElmTest.Runner.Element exposing (runDisplay)
+import Tests
 
 import WebAPI.MathTest
 import WebAPI.NumberTest
@@ -14,22 +14,9 @@ import WebAPI.ScreenTest
 
 main : Signal Element
 main =
-    Signal.map runDisplay tests.signal
-
-
-tests : Mailbox Test
-tests =
-    mailbox (suite "Not arrived yet" [])
+    Signal.map runDisplay (.signal Tests.tests)
 
 
 port task : Task () ()
-port task =
-    sequence
-        [ WebAPI.MathTest.tests
-        , WebAPI.NumberTest.tests
-        , WebAPI.StorageTest.tests
-        , WebAPI.ScreenTest.tests
-        ]
-    `andThen`
-    (send tests.address << suite "Browser tests")
+port task = Tests.task
 
---
+++ test/src/Tests.elm
@@ -0,0 +1,33 @@
+module Tests where
+
+import Signal exposing (Signal, Mailbox, mailbox, constant, send)
+import Task exposing (Task, andThen, sequence)
+import ElmTest.Test exposing (Test, suite)
+
+import WebAPI.MathTest
+import WebAPI.NumberTest
+import WebAPI.StorageTest
+import WebAPI.ScreenTest
+
+
+test : Task () Test
+test =
+    Task.map (suite "WebAPI tests") <|
+        sequence
+            [ WebAPI.MathTest.tests
+            , WebAPI.NumberTest.tests
+            , WebAPI.StorageTest.tests
+            , WebAPI.ScreenTest.tests
+            ]
+
+
+task : Task () ()
+task =
+    test `andThen` send tests.address
+
+
+tests : Mailbox Test
+tests =
+    mailbox (suite "Tests have not arrived yet" [])
+
+
